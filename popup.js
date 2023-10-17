70aadaf54594f857e45f52c4f83519a2d2bee282
jmlee <j0n9m1n1@gmail.com>
2023-10-17 16:38:40 +0900

    working register, option

--- popup.js
+++ popup.js
@@ -28,49 +28,12 @@ chrome.storage.local.get(['titleList'], function (result) {
     }
 });
 
-document.getElementById('clearData').addEventListener('click', function () {
-    chrome.storage.local.remove('titleList', function () {
-        linkListElement.innerHTML = '';
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('openOption').addEventListener('click', function() {
+      chrome.tabs.create({url: 'option.html'});
     });
-});
-
-document.getElementById('exportData').addEventListener('click', function () {
-    chrome.storage.local.get(['titleList'], function (result) {
-        var titleList = result.titleList || [];
-        var jsonData = JSON.stringify(titleList, null, 2);
-
-        var blob = new Blob([jsonData], { type: 'application/json' });
-        var url = URL.createObjectURL(blob);
-
-        var a = document.createElement('a');
-        a.href = url;
-        a.download = 'exported_data.json';
-        document.body.appendChild(a);
-        a.click();
-
-        document.body.removeChild(a);
+  
+    document.getElementById('openRegister').addEventListener('click', function() {
+      chrome.tabs.create({url: 'register.html'});
     });
-});
-
-document.getElementById('uploadButton').addEventListener('click', function () {
-    var fileInput = document.getElementById('fileInput');
-    var file = fileInput.files[0];
-
-    if (file) {
-        var reader = new FileReader();
-
-        reader.onload = function (event) {
-            var content = event.target.result;
-            try {
-                var jsonData = JSON.parse(content);
-                chrome.storage.local.set({ 'titleList': jsonData }, function () {
-                    alert('데이터가 저장되었습니다.');
-                });
-            } catch (error) {
-                alert('올바른 JSON 형식이 아닙니다.');
-            }
-        };
-
-        reader.readAsText(file);
-    }
-});
+  });
---
+++ register.js
@@ -0,0 +1,20 @@
+document.getElementById('registerForm').addEventListener('submit_register', function(event) {
+    event.preventDefault(); // 기본 제출 동작 방지
+  
+    var formData = new FormData(this); // 폼 데이터 가져오기
+  
+    // 서버로 데이터 전송
+    fetch('http://jmlee4dev.net/manatoki_extension/register', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('서버 응답:', data);
+      // 성공적으로 처리된 경우의 코드
+    })
+    .catch(error => {
+      console.error('에러:', error);
+      // 에러 발생 시 처리하는 코드
+    });
+  });
