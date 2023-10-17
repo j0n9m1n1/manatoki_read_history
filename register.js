6f67a3bc3d23c97d9e4e31e2949c9ab421cf83c9
jmlee <j0n9m1n1@gmail.com>
2023-10-17 18:59:00 +0900

    working register

--- register.js
+++ register.js
@@ -1,20 +1,25 @@
-document.getElementById('registerForm').addEventListener('submit_register', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('registerForm').addEventListener('submit', function(event) {
     event.preventDefault(); // 기본 제출 동작 방지
-  
+
     var formData = new FormData(this); // 폼 데이터 가져오기
-  
+
     // 서버로 데이터 전송
     fetch('https://jmlee4dev.net/extension/register', {
-      method: 'POST',
-      body: formData
+        method: 'POST',
+        body: formData
     })
     .then(response => response.json())
     .then(data => {
-      console.log('서버 응답:', data);
-      // 성공적으로 처리된 경우의 코드
+        console.log('서버 응답:', data);
+        // 성공적으로 처리된 경우의 코드
     })
     .catch(error => {
-      console.error('에러:', error);
-      // 에러 발생 시 처리하는 코드
+        console.error('에러:', error);
+        // 에러 발생 시 처리하는 코드
     });
   });
+
+
+    // 여기에 코드 작성
+});
