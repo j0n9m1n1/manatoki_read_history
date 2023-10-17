8700d9941749c9c30cc1a6f41f9605f7fc094892
j0n9m1n1 <j0n9m1n1@gmail.com>
2023-10-18 01:03:55 +0900

    working register error 422

--- register.js
+++ register.js
@@ -1,25 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('registerForm').addEventListener('submit', function(event) {
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('registerForm').addEventListener('submit', function (event) {
     event.preventDefault(); // 기본 제출 동작 방지
 
-    var formData = new FormData(this); // 폼 데이터 가져오기
+    var formData = {
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value,
+      email: document.getElementById('email').value
+    };
 
+    // 데이터를 JSON으로 변환
+    var jsonData = JSON.stringify(formData);
+    console.log(jsonData)
     // 서버로 데이터 전송
     fetch('https://jmlee4dev.net/extension/register', {
-        method: 'POST',
-        body: formData
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData) // 이 부분을 수정하여 JSON으로 변환
+      // body: jsonData
     })
-    .then(response => response.json())
-    .then(data => {
+      .then(response => response.json())
+      .then(data => {
         console.log('서버 응답:', data);
         // 성공적으로 처리된 경우의 코드
-    })
-    .catch(error => {
+      })
+      .catch(error => {
         console.error('에러:', error);
         // 에러 발생 시 처리하는 코드
-    });
+      });
   });
-
-
-    // 여기에 코드 작성
 });
