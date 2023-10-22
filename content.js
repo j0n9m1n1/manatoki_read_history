9a38ada648ba9b9363d62bbad8061a0c4d2def45
j0n9m1n1 <j0n9m1n1@gmail.com>
2023-10-22 20:13:37 +0900

    저쪽 서버에는 있는데 내 서버에 없을 때 작업중

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 //모든 사람이 목록 페이지를 거쳐서 뷰페이지로 이동하지 않음
 //이러나 저러나 크롤링 하면 더 정확해지긴 하는데, 그러고 싶지는 않음
+//서버에 이미 저장 된 것, 안 된 것
+// for listall{ for json }
 function appendReadTime(comic_title) {
 
     chrome.storage.local.get(['token', 'token_expire'], async function (result) {
@@ -14,28 +16,36 @@ function appendReadTime(comic_title) {
                 chrome.runtime.sendMessage({ action: "getReadHistoryOfTitle", comic_title: comic_title, token: token }, function (response) {
                     // 서버 응답을 받아 처리합니다.
                     console.log(response);
-
-                    var listItems = document.querySelectorAll('.list-item'); // 모든 list-item 엘리먼트를 가져옵니다.
-
-                    listItems.forEach(function (listItem) {
+                    // var listItemsWithoutGray = document.querySelectorAll('.list-item:not(.bg-gray)');
+                    // var listItemWithGray = document.querySelectorAll('.list-item.bg-gray');
+                    var listItemsAll = document.querySelectorAll('.list-item'); // 모든 list-item 엘리먼트를 가져옵니다.
+                    // console.log('listGrayItems: ' + listItemWithGray)
+                    // console.log('listGrayItems: ' + listItemWithGray.length)
+                    listItemsAll.forEach(function (listItem) {
                         var dataIndexValue = listItem.getAttribute('data-index');
                         var wrTitle = listItem.querySelector('.wr-subject a').textContent.trim();
                         var countElement = listItem.querySelector('.count.orangered.hidden-xs');
                         var commentCountLength = countElement.textContent.length;
 
                         wrTitle = wrTitle.substring(commentCountLength, wrTitle.length - commentCountLength).trim()
+
                         for (let i = 0; i < response.length; i++) {
                             let item = response[i];
                             if (item.title === wrTitle) {
                                 var titleElement = listItem.querySelector('li[data-index="' + dataIndexValue + '"] .wr-subject');
                                 var timeElement = document.createElement('span');
 
-                                timeElement.textContent = ' Read - ' + item.read_at;
+                                timeElement.textContent = ' Read - ' + item.read_at + ' - ' + item.saved;
                                 titleElement.append(timeElement);
+                                break;
                             }
                         }
-                    });
+                        //왜 break 안되는 느낌
+                        if (listItem.classList.contains('bg-gray')) {
+                            console.log("i'm gray, but not in response", listItem, wrTitle)
+                        }
 
+                    });
                 });
             } else {
                 console.log('토큰이 만료되었습니다.');
