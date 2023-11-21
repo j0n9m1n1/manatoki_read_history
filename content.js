977d775b0d6939c0b9fa4daa511d25e2cc686293
jmlee <j0n9m1n1@gmail.com>
2023-11-21 23:06:27 +0900

    append_read_time() 순서 및 알맞게 요청 값 추가

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ styleElement.textContent = `
 `;
 document.head.appendChild(styleElement);
 
-function append_read_time(comic_title) {
+function append_read_time(comic_title, comic_sid) {
 
     chrome.storage.local.get(['token', 'token_expire'], async function (result) {
         const token = result.token;
@@ -21,24 +21,36 @@ function append_read_time(comic_title) {
 
             if (currentTime < expireTime) {
                 const sidValue = document.querySelector('input[name="sid"]').value;
+                console.log("comic_title"+comic_title);
+                console.log("sidValue" + sidValue);
+                console.log("token" + token);
                 chrome.runtime.sendMessage({ action: "get_history_of_title", comic_title: comic_title, sid: sidValue, token: token }, function (response) {
-                    // 서버 응답을 받아 처리합니다.
                     console.log("append_read_time: " + response);
-                    if (response === "not found episodes") {
-                        //아직 몰루
-                    }
-                    else {
-                        var listItemsAll = document.querySelectorAll('.list-item'); // 모든 list-item 엘리먼트를 가져옵니다.
-                        listItemsAll.forEach(function (listItem) {
-                            var dataIndexValue = listItem.getAttribute('data-index');
-                            var wrTitle = listItem.querySelector('.wr-subject a').textContent.trim();
-                            var countElement = listItem.querySelector('.count.orangered.hidden-xs');
-                            var commentCountLength = countElement.textContent.length;
+                    //extension server에 episode가 없더라도 mana server에는 이미 읽은 것이 있을 수 있어서 확인은 해야함
+                    //response가 없어도 모든 list 순회 > 아래 response.length로 도는 for문에 if exist_episode 
 
-                            wrTitle = wrTitle.substring(commentCountLength, wrTitle.length - commentCountLength).trim()
+                    //이 아래 부분 분명히 개선의 여지가 있어 보이는데 
+                    console.log('get_history_of_title: ' + response)
 
-                            var found = false;
+                    var listItemsAll = document.querySelectorAll('.list-item'); // 모든 list-item 엘리먼트를 가져옵니다.
+                    listItemsAll.forEach(function (listItem) {
+                        var dataIndexValue = listItem.getAttribute('data-index');
+                        var wrTitle = listItem.querySelector('.wr-subject a').textContent.trim();
+                        var countElement = listItem.querySelector('.count.orangered.hidden-xs');
+                        var commentCountLength = countElement.textContent.length;
+                        //episode_sid 얻기
+                        var href = listItem.querySelector('a').getAttribute('href');
+                        var match = href.match(/comic\/(\d+)/);
+                        var episode_sid = ""
+                        if (match) {
+                            episode_sid = match[1];
+                        }
 
+                        wrTitle = wrTitle.substring(commentCountLength, wrTitle.length - commentCountLength).trim()
+
+                        var found = false;
+                        if (response !== "not found episodes") {
+                            // console.log('get_history_of_title: ' + response)
                             for (let i = 0; i < response.length; i++) {
                                 let item = response[i];
                                 if (item.title === wrTitle) {
@@ -58,24 +70,32 @@ function append_read_time(comic_title) {
                                     break;
                                 }
                             }
-                            //이미 읽은건데 extension db에는 없는 경우, 
-                            if (found === false && listItem.classList.contains('bg-gray')) {
-                                console.log("i'm gray, but not in response", listItem, wrTitle)
-                                try {
-                                    // const response = await new Promise((resolve, reject) => {
-                                    // await new Promise((resolve) => {
-                                    chrome.runtime.sendMessage({ action: "add_history", title: wrTitle, read_at: "1970-01-01 00:00:00" }, function (response) {
-                                        // resolve(response);
-                                        // 응답 받으면 여기서 또 받은걸 append해주면 될 듯
-                                    });
-                                    // });
-                                } catch (error) {
-                                    console.error(error);
-                                }
-                            }
+                        }
 
-                        });
-                    }
+                        //이미 읽은건데 extension db에는 없는 경우, 해당 title request, save 
+                        if (found === false && listItem.classList.contains('bg-gray')) {
+                            // console.log("i'm gray, but not in response", listItem, wrTitle)
+                            console.log("i'm gray, but not in response", wrTitle)
+                            try {
+                                // const response = await new Promise((resolve, reject) => {
+                                // await new Promise((resolve) => {
+                                chrome.runtime.sendMessage({ action: "add_history", title: wrTitle, sid: comic_sid, episode_sid: episode_sid, read_at: "1970-01-01 00:00:00" }, function (response) {
+                                    // resolve(response);
+                                    // 응답 받으면 여기서 또 받은걸 append해주면 될 듯
+
+                                    var titleElement = listItem.querySelector('li[data-index="' + dataIndexValue + '"] .wr-subject');
+                                    var timeElement = document.createElement('span');
+
+                                    timeElement.textContent = ' read_at: ' + "1970-01-01 00:00:00";
+                                    titleElement.append(timeElement);
+                                });
+                                // });
+                            } catch (error) {
+                                console.error(error);
+                            }
+                        }
+
+                    });
                 });
             } else {
                 console.log('토큰이 만료되었습니다.');
@@ -96,20 +116,21 @@ async function start() {
 
         if (subjectMeta !== null) {
             subjectValue = subjectMeta.getAttribute('content');
-            console.log('ogTypeValue: ' + ogTypeValue);
+            // console.log('ogTypeValue: ' + ogTypeValue);
 
             var ogUrl = document.querySelector('meta[property="og:url"]');
             var ogUrlValue = ogUrl.getAttribute('content');
 
             if (ogTypeValue === "website") {
-                console.log("목록 페이지");
-                console.log('subject content: ' + subjectValue);
-                append_read_time(subjectValue);
+                // console.log("목록 페이지");
+                // console.log('subject content: ' + subjectValue);
+                var comid_sid = document.querySelector('input[name="sid"]').value;
+                append_read_time(subjectValue, comid_sid);
             }
 
             else if (ogTypeValue === "article") {
-                console.log("뷰 페이지");
-                console.log('subject content: ' + subjectValue);
+                // console.log("뷰 페이지");
+                // console.log('subject content: ' + subjectValue);
 
                 try {
                     const ogImageTag = document.querySelector('meta[property="og:image"]');
@@ -121,8 +142,8 @@ async function start() {
                     if (matches) {
                         comic_sid = matches[1];
                         episode_sid = matches[2];
-                        console.log("첫 번째 값:", comic_sid);
-                        console.log("두 번째 값:", episode_sid);
+                        // console.log("첫 번째 값:", comic_sid);
+                        // console.log("두 번째 값:", episode_sid);
                     } else {
                         console.error("값을 찾을 수 없습니다.");
                     }
@@ -135,8 +156,7 @@ async function start() {
                         // } else if (response.fail) {
                         //     reject(new Error('저장 실패'));
                         // }
-                        console.log("response: " + response)
-                        console.log('finding tag')
+                        console.log("add_history")
 
                         var toonTitleElement = document.querySelector('.toon-title');
 
